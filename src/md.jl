c72081df8498e296e21e8b6ddb07036215bfac19
Your Name <you@example.com>
2025-10-13 00:06:32 +0000

    thermostat not working

--- src/md.jl
+++ src/md.jl
@@ -2,6 +2,7 @@ using Unitful
 using StaticArrays
 using ForwardDiff
 using LinearAlgebra
+using Random, Distributions
 using Tullio
 using GLMakie
 using CUDA, KernelAbstractions, Zygote
@@ -135,16 +136,39 @@ begin
         end
         new_forces = new_forces |> collect
 
-        # Step 3: Update velocities using average of old and new forces
+        # Step 3: Update dt/2 velocities using average of old and new forces
         for i in eachindex(particles)
             old_acceleration = forces[i] / particles[i].mass
             new_acceleration = new_forces[i] / particles[i].mass
             average_acceleration = (old_acceleration + new_acceleration) / 2
             average_acceleration = ustrip.(u"kJ *nm^-1 *mol^-1 *u^-1", average_acceleration) * u"nm/ps^2"
-            particles[i].velocity += average_acceleration .* dt
+            particles[i].velocity += average_acceleration .* (dt / 2)
+        end
+
+        # Step 4: Update dt/2 velocities using Andersen thermostat
+        collision_frequency = 2.0u"ps^-1"
+        @tullio kinetic_energy := 0.5 * particles[i].mass * sum(abs2, particles[i].velocity) # in nm^2 u ps^-2
+        println(kinetic_energy)
+        println(any(isnan, [v.velocity for v in particles] |> Iterators.flatten |> collect))
+        mean_temperature = kinetic_energy / ((3 / 2) * length(particles) * boltzmann_constant)
+        # println(mean_temperature)
+        for i in eachindex(particles)
+            if rand() < collision_frequency * dt
+                T = ustrip(u"K * nm^2 * u * J^-1 * ps^-2", mean_temperature)
+                particles[i].velocity = SVector{3,typeof(1.0u"nm/ps")}(([rand(Distributions.Normal(0, sqrt(T))) for _ in 1:3] .* u"nm/ps")...)
+                if any(isnan, particles[i].velocity)
+                    println(particles[i].velocity)
+                end
+                
+            else
+                old_acceleration = forces[i] / particles[i].mass
+                new_acceleration = new_forces[i] / particles[i].mass
+                average_acceleration = (old_acceleration + new_acceleration) / 2
+                average_acceleration = ustrip.(u"kJ *nm^-1 *mol^-1 *u^-1", average_acceleration) * u"nm/ps^2"
+                particles[i].velocity += average_acceleration .* (dt / 2)
+            end
         end
         forces .= new_forces
-
 
         @tullio kinetic_energy := 0.5 * particles[i].mass * sum(abs2, particles[i].velocity) # in nm^2 u ps^-2
         kinetic_energy = kinetic_energy.val * 1.66053906660e-21 # in kJ/mol
@@ -220,7 +244,7 @@ begin
         traj = []
         prev_ts = time()
         while true
-            
+
             if time() - prev_ts > 1.0
                 positions[] .= reduce(hcat, [[uconvert(u"Å", x).val for x in particle.position] for particle in particles])
                 notify(positions)
@@ -248,6 +272,5 @@ begin
         end
     end
 end
-ssssssssss
 using Serialization
 serialize("traj_gpu.jls", Dict("traj" => traj))
